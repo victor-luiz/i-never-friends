08904f517470e1231755fad8ee9a6971f66abc0c
victor-luiz <luizvic.2112@gmail.com>
2022-07-19 16:55:02 -0300

    edit home and card

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,14 +1,27 @@
-import { IonPage, IonContent, IonApp, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+import { IonPage, IonContent, IonTitle, IonTextarea, IonButton } from '@ionic/react';
+import './Card.css'
+
 const Card: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-      <IonToolbar>
-          <IonTitle>Eu nuca:</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        Eu nunca dei pt
+      <IonContent color={'dark'} fullscreen>
+        <IonTitle color={'warning'}>Eu nunca...</IonTitle>
+        <div className="message">
+          <IonTextarea
+            color={'medium'}
+            readonly={true}
+            autoGrow={true}
+            value={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'} 
+          />
+        </div>
+        <IonButton
+          id="btnNext"
+          shape='round' 
+          size='large' 
+          expand='block'     
+        >
+          Proximo
+        </IonButton>
       </IonContent>
     </IonPage>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,58 @@
-import { IonButton, IonContent , IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonModal, IonPage, IonToolbar, IonButtons, IonIcon, IonItem, IonInput } from '@ionic/react';
+import { add, arrowForward, close, trashBin } from 'ionicons/icons'
+import { useRef, useState } from 'react';
+
+
 import './Home.css'
 
 const Home: React.FC = () => {
+  const [friends, setFriends] = useState<string[]>([''])
+
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  function dismiss() {
+    modal.current?.dismiss();
+  }
+
+  function addNewFriend() {
+    setFriends([...friends, '']);
+  }
+
+  function removeFriend(position: number) {
+    const updateFriends = friends.filter((value, index) => position !== index);
+    
+    setFriends(updateFriends);
+  }
+
+  function handleSaveFriend(position: number, value: string) {
+    const updateFriends = friends.map((friend, index) => {
+      if (index === position) {
+        return value
+      } else {
+        return friend
+      }
+    });
+
+    setFriends(updateFriends);
+  }
+
   return (
     <IonPage>
       <IonContent color={'dark'} fullscreen>
         <main className='content'>
           <span>Eu Nunca</span>
-          <div>
+          <div className='buttons'>
             <IonButton 
               shape='round' 
               size='large' 
               expand='block' 
               color={'warning'}
+              routerLink="/card"
             >
               Iniciar
             </IonButton>
             <IonButton 
+              id='open-modal'
               shape='round' 
               size='large' 
               expand='block' 
@@ -30,6 +66,52 @@ const Home: React.FC = () => {
         <footer>
           <span>Power by Victor</span>
         </footer>
+        <IonModal id="example-modal" ref={modal} trigger="open-modal">
+          <IonContent color={'dark'}>
+            <IonToolbar color={'warning'}>
+              <IonButtons>
+                <IonButton color="dark" onClick={dismiss}>
+                  <IonIcon icon={close} />
+                </IonButton>
+              </IonButtons>
+              <IonButtons slot="end">
+                <IonButton slot='end' color="dark" onClick={dismiss} routerLink="/card">
+                  <IonIcon icon={arrowForward} />
+                </IonButton>
+              </IonButtons>
+            </IonToolbar>
+            {friends.map((friend, index) => {
+              return (
+                <IonItem key={index} color={'dark'}>
+                  <IonInput
+                    name='friend'
+                    placeholder='Digite o nome'
+                    value={friend}
+                    onIonChange={event => handleSaveFriend(index, event.detail.value!)}
+                  />
+                  <IonButtons slot='end'>
+                    <IonButton onClick={addNewFriend}>
+                      <IonIcon
+                        icon={add} 
+                        color={'success'}
+                      />
+                    </IonButton>
+                    <IonButton
+                      disabled={friends.length < 2}
+                      onClick={() => removeFriend(index)}
+                      slot="icon-only"
+                    >
+                      <IonIcon
+                        icon={trashBin} 
+                        color={'danger'}
+                      />
+                    </IonButton>
+                  </IonButtons>
+                </IonItem>
+              );
+            })}
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
